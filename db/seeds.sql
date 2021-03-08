fe6a4bd0883c25525c13093e0d5abc74ce45e0d6
Mike Carter <elite.mikecarter@gmail.com>
2021-03-08 10:53:45 -0500

    troubleshooting

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO burgers (name) VALUES ('Happy Burger');
-INSERT INTO burgers (name) VALUES ('Happy Burger XL');
-INSERT INTO burgers (name, devoured) VALUES ('Double Happy Burger', true);
-INSERT INTO burgers (name, devoured) VALUES ('Baby Happy burger', true);
-INSERT INTO burgers (name, devoured) VALUES ('Beyond Happy Burger', true);
-INSERT INTO burgers (name) VALUES ('HappyBurger Supreme');
+INSERT INTO burgers (burger_name) VALUES ('Happy Burger');
+INSERT INTO burgers (burger_name) VALUES ('Happy Burger XL');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Happy Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Baby Happy burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beyond Happy Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('HappyBurger Supreme');
